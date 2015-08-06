0b8f4a28e83c25c313747d2618178375c44408c8
Laura Fava <laura@blueroom.local>
2015-08-05 17:12:33 -0700

    ex1.py is the python version of exercise 1 for th coursera machine learning course

---
+++ week2/ex1/ex1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# ex1.py is a modified version of ex1.m from the coursera machine learning course
+
+# First exercise: Define a function to call to five the 5D identity matrix
+
+import sys
+import os
+
+def warmUpExercise():
+  print '6'
+  return
+
+def main():
+
+  warmUpExercise()
+
+  return
+
+if __name__=="__main__":
+  main()
