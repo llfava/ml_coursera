3e5e169e4abc4b0d9c8ea7d792c531ba359e9c23
Laura Fava <laura@blueroom.local>
2015-08-17 14:13:55 -0700

    Completed ex1.py, but have errors:  theta0 and theta1 are too high (by orders of magnitude)  contour is tilted

--- week2/ex1/ex1.py
+++ week2/ex1/ex1.py
@@ -7,6 +7,8 @@ import sys
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 import copy
 
 # warmUpExercise function to return the 5x5 identity matrix
@@ -58,7 +60,7 @@ def part2_1():
   data = np.genfromtxt("ex1data1.txt", delimiter=',') #Read in comma-separated data
   X, y = data[:,0], data[:,1]
   m = len(y)
-  plotData(X,y)
+#  plotData(X,y)
   return data
 
 def part2_2(data):
@@ -76,15 +78,53 @@ def part2_2(data):
 
   predict1 = np.array([1,3.5]).dot(theta)
   predict2 = np.array([1,7]).dot(theta)
-  print predict1
-  print predict2
+#  print predict1
+#  print predict2
+
+#  print y
+#  print theta
 
   #fig = plt.figure()
   #ax1 = plt.subplot(111)
 
-  plt.plot(X[:,1], y, 'rx')
+#  plt.plot(X[:,1], y, 'rx')
   plt.plot(X[:,1], X.dot(theta), 'b-')
+#  plt.show()
+
+def part2_4():
+  data = np.genfromtxt("ex1data1.txt", delimiter=',') #Read in comma-separated data
+  X, y = data[:,0], data[:,1]
+  m = len(y)
+  y = y.reshape(m,1)
+  X = np.c_[np.ones(m),data[:,0]]
+
+  #Grid over which we calculate J
+  theta0_vals = np.linspace(-10, 20, 100)
+  theta1_vals = np.linspace(-1, 10, 100)
+
+  #Initialize J_vals to a matrix of zeros
+  J_vals = np.zeros((len(theta0_vals), len(theta1_vals))) 
+ 
+  #Fill out J_vals
+  for i, _ in enumerate(theta0_vals):
+    for j, _ in enumerate(theta1_vals):
+      theta = np.array((theta0_vals[i], theta1_vals[j])).reshape(2,1)
+      J_vals[i,j] = computeCost(X, y, theta)
+
+  print theta[0], theta[1]
+
+
+  R, P = np.meshgrid(theta0_vals, theta1_vals) #Creates grid with these values
+  
+  fig = plt.figure()
+  ax1 = fig.add_subplot(2,1,1, projection='3d') #Need Axes3D for this to work
+  ax1.plot_surface(R, P, J_vals)
+
+  ax2 = fig.add_subplot(2,1,2)
+  ax2.contour(R,P,J_vals, np.logspace(-2,3,20))
+  ax2.plot(theta[0], theta[1], 'rx', markersize = 10)
   plt.show()
+
 
 def main():
   n = 5
@@ -92,6 +132,7 @@ def main():
 
   data = part2_1()
   part2_2(data)
+  part2_4()
   return
 
 if __name__=="__main__":
