7dace4165357f90671cdd3dbd2885d05c6db9fce
Laura Fava <laura@blueroom.local>
2015-08-31 15:14:18 -0700

    Added sigmoid function

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -11,7 +11,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def part1_1(data):
+  pos = data[data[:,2] == 1] #Select X data for y=1
+  neg = data[data[:,2] == 0] #Select X data for y=0
 
+  ax1 = plt.subplot(111)
+  ax1.set_xlabel("Exam 1 score")
+  ax1.set_ylabel("Exam 2 score")
+  ax1.scatter(pos[:,0], pos[:,1], s=40, c='k', marker='+', label="Admitted")
+  ax1.scatter(neg[:,0], neg[:,1], s=40, c='y', marker='o', label="Not admitted")
+  ax1.legend()
+#  plt.show()
+
+def sigmoid(z):
+  g = 1.0 / (1.0 + np.exp(-z))
+  return g
 
 def main():
 
@@ -22,17 +36,10 @@ def main():
   X = np.c_[np.ones(m), data[:,:n]]
   y = data[:,n:n+1] #TODO: Note difference between data[:,n] and data[:,n:n+1] - why is this?
 
-  pos = data[data[:,2] == 1]
-  neg = data[data[:,2] == 0]
-
-  ax1 = plt.subplot(111)
-  ax1.set_xlabel("Exam 1 score")
-  ax1.set_ylabel("Exam 2 score")
-  ax1.scatter(pos[:,0], pos[:,1], s=40, c='k', marker='+', label="Admitted")
-  ax1.scatter(neg[:,0], neg[:,1], s=40, c='y', marker='o', label="Not admitted")
-  ax1.legend()
-  plt.show()
-
+  part1_1(data) #Plot the data
+  
+  g = sigmoid(data)
+  print g
 
 if __name__ == "__main__":
   main()
