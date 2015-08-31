a9c4ed1cf3050fe94b1cbf663926da80f89bf520
Laura Fava <laura@blueroom.local>
2015-08-31 16:06:00 -0700

    Completed part 1.2.1 and 1.2.2; wrote sigmoid function and functions for calculating cost function and gradient of cost function

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def part1_1(data):
+def plotData(data):
   pos = data[data[:,2] == 1] #Select X data for y=1
   neg = data[data[:,2] == 0] #Select X data for y=0
 
@@ -21,25 +21,52 @@ def part1_1(data):
   ax1.scatter(pos[:,0], pos[:,1], s=40, c='k', marker='+', label="Admitted")
   ax1.scatter(neg[:,0], neg[:,1], s=40, c='y', marker='o', label="Not admitted")
   ax1.legend()
-#  plt.show()
+
+def part1_1(): #Plot the data
+  data = np.genfromtxt("ex2data1.txt", delimiter=',') #Read in comma separated data
+  plotData(data) 
 
 def sigmoid(z):
   g = 1.0 / (1.0 + np.exp(-z))
   return g
 
-def main():
+def computeCost(theta, x, y):
+  m = len(y)
+  sum = np.log(sigmoid(x.dot(theta))).T.dot(-y)-np.log(1-sigmoid(x.dot(theta))).T.dot(1-y)
+  return (sum / m).flatten()
 
-  ## Load data; first two columns contains the exam scores, and the third column
-  ## contains the label
+def calcGradient(theta, x, y):
+  m = len(y)
+  sum = x.T.dot(sigmoid(x.dot(theta))-y)
+  return (sum / m).flatten()
+
+def part1_2():
   data = np.genfromtxt("ex2data1.txt", delimiter=',') #Read in comma separated data
   m, n = np.shape(data)[0], np.shape(data)[1]-1
   X = np.c_[np.ones(m), data[:,:n]]
   y = data[:,n:n+1] #TODO: Note difference between data[:,n] and data[:,n:n+1] - why is this?
+  theta = np.zeros((n+1,1))
 
-  part1_1(data) #Plot the data
+  print computeCost(theta,X,y)
+  print calcGradient(theta,X,y)
+
+def main():
+
+  ## Load data; first two columns contains the exam scores, and the third column
+  ## contains the label
+
+  part1_1()
+#  plt.show()
+
+  part1_2()
+
+
+
+
+
   
-  g = sigmoid(data)
-  print g
+
+
 
 if __name__ == "__main__":
   main()
