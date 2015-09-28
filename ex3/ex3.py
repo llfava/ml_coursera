1e83dbd6996e331f1d2047bd3edfb02377c5024a
Laura Fava <laura@blueroom.local>
2015-09-28 15:31:51 -0700

    Added costFunction()

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -9,7 +9,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io #module to load files formatted for other languages (here for matlab files)
-import scipy.misc
+import scipy.misc #module containing function to display image from numpy array
+import scipy.special #for logistic function
 
 def displayData(X):
   width = 20
@@ -31,7 +32,35 @@ def displayData(X):
   axes    = figure.add_subplot(111)
   axes.imshow( img )
 
-  plt.show()
+  #plt.show()
+
+def sigmoid(z):
+#  g = 1.0 / (1.0 + np.exp(-z))
+  g = scipy.special.expit(z)
+  return g
+
+def computeCost(theta, X, y):
+  m = len(y)
+  hypo = sigmoid(X.dot(theta.T))
+  term1 = np.log(hypo).T.dot(-y)
+  term2 = np.log(1-hypo).T.dot(1-y)
+  sum = term1 - term2
+
+  return (sum / m).flatten()
+
+def gradientCost(theta, X, y):
+  m = len(y)
+  sum = X.T.dot(sigmoid(X.dot(theta))-y)
+  return (sum / m).flatten()
+
+def costFunction(theta, X, y):
+  cost = computeCost(theta,X,y)
+  grad = gradientCost(theta,X,y)
+  return cost
+
+######################
+# Parts of assignment
+######################
 
 #Load matlab data file
 def part1_1(): 
@@ -43,12 +72,29 @@ def part1_2():
   mat = scipy.io.loadmat('ex3data1')
   X, y = mat['X'], mat['y']
   displayData(X)  
+
+#Vectorizing logistic regression
+def part1_3():  
+  mat = scipy.io.loadmat('ex3data1')
+  X, y = mat['X'], mat['y']
+  m = len(y)
+  X = np.c_[np.ones( (m,1) ), X]
+
+  num_pixels = np.shape(X)[1]
+  num_classes = 10
+
+  theta = np.zeros( (num_classes,num_pixels) )
+
+
+
+  print computeCost(theta, X, y)  
   
 
 def main():
 
-  part1_1()
+#  part1_1()
   part1_2()
+  part1_3()
 
 if __name__ == "__main__":
   main()
